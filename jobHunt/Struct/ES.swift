0fa68f8f6a78c5d63587b4befa5a4ee5fc0967d4
maple-fm <fuma.deguchi@gmail.com>
2022-10-10 01:41:41 +0900

    ESの構造体を追加

---
+++ jobHunt/Struct/ES.swift
@@ -0,0 +1,20 @@
+//
+//  ES.swift
+//  jobHunt
+//
+//  Created by 出口楓真 on 2022/10/10.
+//
+
+import Foundation
+
+struct ES {
+    var name: String = ""
+    var deadline: Date = .now
+    var motivation: String = ""
+    var gakuchika: String = ""
+    var strongPoints: String = ""
+    var weakPoints: String = ""
+    var other: String = ""
+    var isClick: Bool = false
+
+}
