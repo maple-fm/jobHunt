db549275781a054f084239855807b3f779d26551
maple-fm <fuma.deguchi@gmail.com>
2022-10-10 01:43:28 +0900

    InterView, Sessionの構造体を追加

---
+++ jobHunt/Struct/InterView.swift
@@ -0,0 +1,21 @@
+//
+//  InterView.swift
+//  jobHunt
+//
+//  Created by 出口楓真 on 2022/10/10.
+//
+
+import Foundation
+
+struct Interview {
+    var name = ""
+    var deadline: Date = .now
+    var location = ""
+    var clothes = ""
+    var motivation = ""
+    var gakuchika = ""
+    var strongPoints = ""
+    var weakPoints = ""
+    var other = ""
+
+}
---
+++ jobHunt/Struct/Session.swift
@@ -0,0 +1,19 @@
+//
+//  Session.swift
+//  jobHunt
+//
+//  Created by 出口楓真 on 2022/10/10.
+//
+
+import Foundation
+
+struct Session {
+    var name = ""
+    var deadline: Date = .now
+    var location = ""
+    var clothes = ""
+    var item = ""
+    var questions = ""
+    var other = ""
+
+}
