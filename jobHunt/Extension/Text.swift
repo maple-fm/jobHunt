21f5fc132148ad09529bf53ad503c18060a1cc55
maple-fm <fuma.deguchi@gmail.com>
2022-10-10 02:11:35 +0900

    Extensionの追加

---
+++ jobHunt/Extension/Text.swift
@@ -0,0 +1,17 @@
+//
+//  Text.swift
+//  jobHunt
+//
+//  Created by 出口楓真 on 2022/10/10.
+//
+import Foundation
+import SwiftUI
+
+extension Text {
+    func headetTitle() -> some View {
+        self
+            .font(.title)
+            .foregroundColor(.black)
+            .padding(.bottom, 5)
+    }
+}
---
+++ jobHunt/Extension/TextField.swift
@@ -0,0 +1,23 @@
+//
+//  TextField.swift
+//  jobHunt
+//
+//  Created by 出口楓真 on 2022/10/10.
+//
+
+import Foundation
+import SwiftUI
+
+extension TextField {
+    func input() -> some View {
+        self
+            .listRowBackground(Color.white)
+    }
+}
+
+extension TextEditor {
+    func input() -> some View {
+        self
+            .listRowBackground(Color.white)
+    }
+}
