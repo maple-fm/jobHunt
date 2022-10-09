eca468f64267906c1d3b6dc89bad53b3fb859a3c
maple-fm <fuma.deguchi@gmail.com>
2022-10-10 01:42:27 +0900

    Internshipの構造体を追加

---
+++ jobHunt/Struct/Internship.swift
@@ -0,0 +1,18 @@
+//
+//  Internship.swift
+//  jobHunt
+//
+//  Created by 出口楓真 on 2022/10/10.
+//
+
+import Foundation
+
+struct Internship {
+    var name = ""
+    var deadline: Date = .now
+    var location = ""
+    var clothes = ""
+    var item = ""
+    var other = ""
+
+}
