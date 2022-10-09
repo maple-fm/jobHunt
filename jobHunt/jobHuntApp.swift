bf6692e2971a5c99527899900e5876eb8bbfd2ba
maple-fm <fuma.deguchi@gmail.com>
2022-10-09 23:28:20 +0900

    Initial Commit

---
+++ jobHunt/jobHuntApp.swift
@@ -0,0 +1,17 @@
+//
+//  jobHuntApp.swift
+//  jobHunt
+//
+//  Created by 出口楓真 on 2022/10/09.
+//
+
+import SwiftUI
+
+@main
+struct jobHuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
