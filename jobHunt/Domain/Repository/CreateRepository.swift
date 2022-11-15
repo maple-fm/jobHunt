f2808330966b84289722c3ffa65e29a396862870
maple-fm <fuma.deguchi@gmail.com>
2022-11-16 02:30:30 +0900

    イベント追加時のバリデーションをRepositoryとして切り出した

---
+++ jobHunt/Domain/Repository/CreateRepository.swift
@@ -0,0 +1,21 @@
+//
+//  CreateRepository.swift
+//  jobHunt
+//
+//  Created by 出口楓真 on 2022/11/16.
+//
+
+import Foundation
+
+struct CreateRepository {
+    // TODO: この関数を一つのファイル内にまとめたい
+    func isValidated(name: String?) -> Bool {
+        guard
+            let name = name
+        else{ return false }
+        let isValidName = !name.isEmpty
+
+        return isValidName
+    }
+    
+}
