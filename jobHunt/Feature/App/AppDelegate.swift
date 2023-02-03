f7efe36dcf675372263aff3b322d14928206d73d
maple-fm <fuma.deguchi@gmail.com>
2023-02-04 00:48:37 +0900

    イベント件数によって通知のメッセージ内容を変更

--- jobHunt/Feature/App/AppDelegate.swift
+++ jobHunt/Feature/App/AppDelegate.swift
@@ -39,18 +39,28 @@ private func notification() {
     let repository = EventRepository()
     let format = FormatRepository()
     let numberOfEvent = repository.getEvents().filter { format.formatDate(date: $0.deadline) == format.formatDate(date: .now) }.count
+    var message: String = ""
 
     do {
         let content = UNMutableNotificationContent()
         let date = DateComponents(hour:9)
 //        let trigger = UNCalendarNotificationTrigger.init(dateMatching: date, repeats: true)
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 80, repeats: true)
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
+
+        if numberOfEvent == 0 {
+            message = "今日の就活はおやすみ!しっかり休んでね!"
+        } else {
+            message =   """
+                        今日は\(numberOfEvent)件の就活があるよ！
+                        気合い入れていこう！
+                        """
+        }
 
 
         //通知内容
         content.title = "今日の就活"
         // TODO: 文章要検討
-        content.body = "今日は\(numberOfEvent)件の就活があるよ！気合い入れていこう！" // 就活0件の場合でメッセージを変える
+        content.body = message
         content.sound = UNNotificationSound.default
         content.badge = 1
         //通知リクエストを作成
