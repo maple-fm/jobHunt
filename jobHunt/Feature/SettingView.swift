69a41298edad2015b9887c88df052dde0e066888
maple-fm <fuma.deguchi@gmail.com>
2023-06-01 21:06:29 +0900

    feat: 設定画面にバナー広告の追加

--- jobHunt/Feature/SettingView.swift
+++ jobHunt/Feature/SettingView.swift
@@ -10,48 +10,55 @@ import SwiftUI
 struct SettingView: View {
     @Environment(\.dismiss) var dismiss
     var body: some View {
-        Form {
-            List {
-                Section(header: Text("お問い合わせ")) {
-                    if let url = URL(string: "https://forms.gle/EN7WjkSiQG3fDwx87") {
-                        Link("お問い合わせ", destination: url)
+        VStack {
+            Form {
+                List {
+                    Section(header: Text("お問い合わせ")) {
+                        if let url = URL(string: "https://forms.gle/EN7WjkSiQG3fDwx87") {
+                            Link("お問い合わせ", destination: url)
+                        }
+
+                        if let url = URL(string: "https://forms.gle/3WRnS3bJ6J4BHj1V6") {
+                            Link("バグ報告", destination: url)
+                        }
                     }
 
-                    if let url = URL(string: "https://forms.gle/3WRnS3bJ6J4BHj1V6") {
-                        Link("バグ報告", destination: url)
+                    Section(header: Text("その他")) {
+                        NavigationLink("このアプリについて", destination: PrivacyAndTerms())
+
                     }
                 }
-
-                Section(header: Text("その他")) {
-                    NavigationLink("このアプリについて", destination: PrivacyAndTerms())
-
-                }
+                .navigationTitle("設定")
             }
-            .navigationTitle("設定")
-        }
-        .navigationBarBackButtonHidden(true)
-        .toolbar {
-            ToolbarItem(placement: .navigationBarLeading) {
-                Button(action: {
-                    dismiss()
-                }) {
-                    HStack {
-                        Image(systemName: "chevron.backward")
-                            .font(.system(size: 17, weight: .medium))
-                        Text("カレンダー")
-                    }
-                }
-            }
-        }
-        .gesture(DragGesture(minimumDistance: 5)
-            .onChanged { value in
-                withAnimation {
-                    if value.translation.width > 100 {
+            .navigationBarBackButtonHidden(true)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button(action: {
                         dismiss()
+                    }) {
+                        HStack {
+                            Image(systemName: "chevron.backward")
+                                .font(.system(size: 17, weight: .medium))
+                            Text("カレンダー")
+                        }
                     }
                 }
             }
-        )
+            .gesture(DragGesture(minimumDistance: 5)
+                .onChanged { value in
+                    withAnimation {
+                        if value.translation.width > 100 {
+                            dismiss()
+                        }
+                    }
+                }
+            )
+
+            BannerView()
+                .frame(height: 60)
+                .padding(.bottom, 50)
+        }
+
 
     }
 }
